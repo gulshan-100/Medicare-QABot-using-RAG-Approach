37c8918834a7ca11f44932a63ac19dc241eb2064
Gulshan Kumar <132364518+gulshan-100@users.noreply.github.com>
2024-09-06 14:37:28 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -1,8 +1,9 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import Pinecone
 from dotenv import load_dotenv, find_dotenv
-from langchain.chains import RetrievalQA  
+from langchain.chains import RetrievalQA
 from langchain_groq import ChatGroq
+from langchain.prompts import PromptTemplate
 
 load_dotenv(find_dotenv())
 
@@ -24,14 +25,27 @@ def create_qa_chain():
         embedding=embedding_model
     )
 
-    # Create and return the RetrievalQA chain
+    # Define a custom prompt template
+    prompt_template = """Use the following pieces of context to answer the question of user in 
+    detail at the end. Solve the user query in best way possible in detailed manner. If you don't know the answer, just say that you don't know, don't try 
+    to make up an answer.
+
+    {context}
+
+    Question: {question}
+    Answer:"""
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    # Create and return the RetrievalQA chain with the custom prompt
     qa_chain = RetrievalQA.from_chain_type(
         llm=groq_model,
         chain_type="stuff",
-        retriever=knowledge.as_retriever()
+        retriever=knowledge.as_retriever(),
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": PROMPT}
     )
     
     return qa_chain
-
-
-
